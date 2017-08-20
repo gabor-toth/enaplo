8f2738a8a4ded77ea8f7c4020aecef9dbdb87f61
Gabor Toth <gabor.toth@softvar.hu>
2017-08-20 13:46:01 +0200

    initial setup

---
+++ enaplo/src/app/enaplo/enaplo.service.ts
@@ -0,0 +1,62 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Enaplo } from './enaplo';
+
+@Injectable()
+export class EnaploService {
+
+  private headers = new Headers({'Content-Type': 'application/json'});
+  private apiUrl = 'api/enaplo';  // URL to web api
+
+  constructor(private http: Http) { }
+
+  getAll(): Promise<Enaplo[]> {
+    return this.http.get(this.apiUrl)
+               .toPromise()
+               .then(response => response.json().data as Enaplo[])
+               .catch(this.handleError);
+  }
+
+
+  get(id: number): Promise<Enaplo> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Enaplo)
+      .catch(this.handleError);
+  }
+
+  delete(id: number): Promise<void> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
+  create(name: string): Promise<Enaplo> {
+    return this.http
+      .post(this.apiUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Enaplo)
+      .catch(this.handleError);
+  }
+
+  update(entity: Enaplo): Promise<Enaplo> {
+    const url = `${this.apiUrl}/${entity.id}`;
+    return this.http
+      .put(url, JSON.stringify(entity), {headers: this.headers})
+      .toPromise()
+      .then(() => entity)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+}
+
