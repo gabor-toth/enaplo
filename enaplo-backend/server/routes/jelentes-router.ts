e46f8b20b558529e1c77cd9140505c95eff2d3e2
Gabor Toth <gabor.toth@softvar.hu>
2017-09-03 21:51:51 +0200

    started jelentes

---
+++ enaplo-backend/server/routes/jelentes-router.ts
@@ -0,0 +1,30 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import { BaseRouter } from './base-router';
+import { FonaploParser, NaploParser } from '../parsers/exports';
+import { ApiResponse } from '../model/api-response';
+
+export class RouterJelentes extends BaseRouter {
+  register( router: Router ) {
+    /*
+    router.get( '/api/enaplok', ( request: Request, response: Response, next: NextFunction ) => {
+      this.proxy( response, '/ajax?method=naplofa_load&id=%23page_enaplok&htmlid=${htmlid}&_=${time}' );
+    } );
+
+    router.post( '/api/enaplok', ( request: Request, response: Response, next: NextFunction ) => {
+      this.parseBody( request, response, new NaploParser() );
+    } );
+
+    router.get( '/api/enaplo/:id', ( request: Request, response: Response, next: NextFunction ) => {
+      const id = request.params.id;
+      if ( !this.checkParameter( response, 'id', id ) ) {
+        return;
+      }
+      this.proxy( response, '/ajax?method=get_naplo_items&parentid=enaploAktaFa&aktaid=${naplosorszam}&htmlid=${htmlid}&_=${time}' );
+    } );
+
+    router.post( '/api/enaplo/:id', ( request: Request, response: Response, next: NextFunction ) => {
+      this.parseBody( request, response, new FonaploParser() );
+    } );
+    */
+  }
+}
