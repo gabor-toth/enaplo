3b6ed5c79171812ea6c88c56d4f72f75c99f0055
Gabor Toth <gabor.toth@softvar.hu>
2017-09-08 20:47:57 +0200

    sending cookies works

--- enaplo-app/chrome-app/chromeapp.js
+++ enaplo-app/chrome-app/chromeapp.js
@@ -1,28 +1,70 @@
-const EXT_EXTENSION_ID = 'eldaofnbiaopigaahkmacaonfpokngce';
+var EnaploChromeApp = (function() {
+	const EXT_EXTENSION_ID = 'eldaofnbiaopigaahkmacaonfpokngce';
+	const DEBUG = false;
 
-function showMainWindow() {
-	chrome.app.window.create('index.html', {
-		'outerBounds' : {
-			'width' : 800,
-			'height' : 600
-		}
-	});
-}
+	function showMainWindow() {
+		chrome.app.window.create('index.html', {
+			'outerBounds' : {
+				'width' : 800,
+				'height' : 600
+			}
+		});
+	}
 
-function requestLoggedInAndCookie() {
-	console.log('requestLoggedInAndCookie');
-	chrome.runtime.sendMessage(EXT_EXTENSION_ID, {
-		requestState : true
-	}, function(state) {
-		if ( state == null ) {
+	function requestStatusCallback(cookiesState) {
+		if ( cookiesState == null ) {
 			console.log('E-Napló extension is not installed or is not enabled, please check it.');
 		} else {
-			console.log(JSON.stringify(state));
+			onMessageCookies(cookiesState);
 		}
-	});
-}
+	}
 
-chrome.app.runtime.onLaunched.addListener(function() {
-	requestLoggedInAndCookie();
-	showMainWindow();
-});
+	function requestStatus() {
+		let request = {
+			requestState : true
+		};
+		chrome.runtime.sendMessage(EXT_EXTENSION_ID, request, requestStatusCallback);
+	}
+
+	function onMessageCookies(cookiesState) {
+		if ( window.angularCookieComponentRef ) {
+			window.angularCookieComponentRef.onCookiesChange(cookiesState);
+		} else {
+			console.warn('No angularCookieComponentRef registered');
+		}
+	}
+
+	function messageListenerCallback(request, sender, sendResponse) {
+		if ( DEBUG ) {
+			console.log("onMessageExternal sender:" + JSON.stringify(sender) + " request: " + JSON.stringify(request));
+		}
+		if ( request.cookies ) {
+			onMessageCookies(request);
+			let response = {
+				processed : true
+			};
+			sendResponse(response);
+		} else {
+			console.log("onMessageExternal unhandled request, sender:" + JSON.stringify(sender) + " request: " + JSON.stringify(request));
+		}
+		return true; // sendResponse will be called asynchronously
+	}
+
+	function registerMessageListener() {
+		chrome.runtime.onMessageExternal.addListener(messageListenerCallback);
+	}
+
+	function onLaunchedCallback() {
+		registerMessageListener();
+		requestStatus();
+		showMainWindow();
+	}
+
+	return {
+		init : function() {
+			chrome.app.runtime.onLaunched.addListener(onLaunchedCallback);
+		}
+	};
+})();
+
+EnaploChromeApp.init();
--- enaplo-ext/src/background.js
+++ enaplo-ext/src/background.js
@@ -4,6 +4,7 @@ const COOKIE_DOMAIN_SUFFIX = '.e-epites.hu';
 const COOKIE_PATH = '/enaplo_demo';
 const COOKIE_NAME_HTML_ID = 'htmlid';
 const COOKIE_NAME_SESSION_ID = 'session_id';
+const DEBUG = false;
 
 function debugLogAllCookies() {
 	let selector = {
@@ -18,43 +19,51 @@ function debugLogAllCookies() {
 	});
 }
 
+function processCookies(allCookies, callback) {
+	let domainCookies = {},
+		cookiesHtmlid = null,
+		cookieSessionid = null;
+	for ( let i in allCookies ) {
+		let cookie = allCookies[ i ];
+		if ( !cookie.domain.endsWith(COOKIE_DOMAIN_SUFFIX) ) {
+			continue;
+		}
+		if ( cookie.name == COOKIE_NAME_SESSION_ID ) {
+			cookieSessionid = cookie.value;
+		} else if ( cookie.name == COOKIE_NAME_HTML_ID ) {
+			cookieHtmlid = cookie.value;
+		}
+		domainCookies[ cookie.name ] = cookie.value;
+	}
+	let isLoggedIn = cookieSessionid != null && cookieSessionid.length != 0 && (cookieHtmlid == null || cookieHtmlid.length == 0);
+	if ( !isLoggedIn ) {
+		cookieSessionid = null;
+	}
+	let state = {
+		cookies : domainCookies,
+		loggedIn : isLoggedIn
+	};
+	if ( DEBUG ) {
+		console.log(JSON.stringify(state));
+	}
+	if ( callback != null ) {
+		callback(state);
+	}
+}
+
 function determineLoggedInAndCookie(callback) {
 	let selector = {
 		domain : COOKIE_DOMAIN_SUFFIX
 	};
 	chrome.cookies.getAll(selector, function(allCookies) {
-		let domainCookies = {},
-			cookiesHtmlid = null,
-			cookieSessionid = null;
-		for ( let i in allCookies ) {
-			let cookie = allCookies[ i ];
-			if ( !cookie.domain.endsWith(COOKIE_DOMAIN_SUFFIX) ) {
-				continue;
-			}
-			if ( cookie.name == COOKIE_NAME_SESSION_ID ) {
-				cookieSessionid = cookie.value;
-			} else if ( cookie.name == COOKIE_NAME_HTML_ID ) {
-				cookieHtmlid = cookie.value;
-			}
-			domainCookies[ cookie.name ] = cookie.value;
-		}
-		let isLoggedIn = cookieSessionid != null && cookieSessionid.length != 0 && (cookieHtmlid == null || cookieHtmlid.length == 0);
-		if ( !isLoggedIn ) {
-			cookieSessionid = null;
-		}
-		let state = {
-			cookies : domainCookies,
-			loggedIn : isLoggedIn
-		};
-		//console.log(JSON.stringify(state));
-		if ( callback != null ) {
-			callback(state);
-		}
+		processCookies(allCookies, callback);
 	});
 }
 
-chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
-	//console.log("onMessageExternal sender:" + JSON.stringify(sender) + " request: " + JSON.stringify(request));
+function onMessageListener(request, sender, sendResponse) {
+	if ( DEBUG ) {
+		console.log("onMessageExternal sender:" + JSON.stringify(sender) + " request: " + JSON.stringify(request));
+	}
 	if ( request.requestState ) {
 		determineLoggedInAndCookie(function(state) {
 			sendResponse(state);
@@ -63,15 +72,32 @@ chrome.runtime.onMessageExternal.addListener(function(request, sender, sendRespo
 		console.log("onMessageExternal unhandled request, sender:" + JSON.stringify(sender) + " request: " + JSON.stringify(request));
 	}
 	return true; // sendResponse will be called asynchronously
-});
+}
 
-chrome.cookies.onChanged.addListener(function(info) {
+function onCookiesCollectedResponse(response) {
+	if ( response == null ) {
+		console.log('E-Napló app is not running');
+	} else {
+		if ( DEBUG ) {
+			console.log(JSON.stringify(response));
+		}
+	}
+}
+
+function onCookiesCollected(state) {
+	chrome.runtime.sendMessage(APP_EXTENSION_ID, state, onCookiesCollectedResponse);
+}
+
+function onCookiesChanged(info) {
 	let cookie = info.cookie;
 	if ( !cookie.domain.endsWith(COOKIE_DOMAIN_SUFFIX) || cookie.path != COOKIE_PATH ) {
 		return;
 	}
-	//console.log("onChanged " + JSON.stringify(info));
-	determineLoggedInAndCookie(function(state) {
-		chrome.runtime.sendMessage(APP_EXTENSION_ID, state, null);
-	});
-});
+	if ( DEBUG ) {
+		console.log("onChanged " + JSON.stringify(info));
+	}
+	determineLoggedInAndCookie(onCookiesCollected);
+}
+
+chrome.cookies.onChanged.addListener(onCookiesChanged);
+chrome.runtime.onMessageExternal.addListener(onMessageListener);
