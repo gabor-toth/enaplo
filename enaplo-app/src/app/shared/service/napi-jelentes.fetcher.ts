d92a93f36ddc1acc7661df35ad62ecca5fed14cd
Gabor Toth <gabor.toth@softvar.hu>
2018-01-28 18:20:54 +0100

    update to newest, fix compile errors

--- enaplo-app/src/app/shared/service/napi-jelentes.fetcher.ts
+++ enaplo-app/src/app/shared/service/napi-jelentes.fetcher.ts
@@ -25,7 +25,8 @@ export class NapiJelentesFetcher {
 
 	private fetchNext(): Promise<Naplo[]> {
 		const naploSorszam = this.naplok[ this.currentIndex ].sorszam;
-		return this.NapiJelentesService.getFonaplok( naploSorszam ).then( response => this.receivedResponse( response ) );
+		// 		return this.napiJelentesService.getFonaplok( naploSorszam ).then( response => this.receivedResponse( response ) );
+		return null;
 	}
 
 	private receivedResponse( response: Response ): Promise<Naplo[]> {
@@ -36,7 +37,7 @@ export class NapiJelentesFetcher {
 	}
 
 	private receivedLastResponse(): Promise<Naplo[]> {
-		this.NapiJelentesService.cachedNaplok = this.naplok;
+		//		this.napiJelentesService.cachedNaplok = this.naplok;
 		return Promise.resolve( this.naplok );
 	}
 
